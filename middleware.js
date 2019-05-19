0351132752332b011c87f2adf39926a58bca0867
Charlie <chen_610146@hotmail.com>
2019-05-19 13:51:19 +0800

    create timeStamps

---
+++ middleware.js
@@ -0,0 +1,12 @@
+
+module.exports = (req, res, next) => {
+  const dateTime = new Date();
+  const year = dateTime.getFullYear()
+  const month = dateTime.getMonth() + 1
+  const day = dateTime.getDate()
+  const hour = dateTime.getHours()
+  const min = dateTime.getMinutes()
+  const sec = dateTime.getSeconds()
+  console.log(`${year}-${month}-${day} ${hour}:${min}:${sec} | ${req.method} from ${req.url}`)
+  next()
+}
